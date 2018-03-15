2519a398128d9a47f69ac52b39b20ddce4fd01e8
Sergey Struk <siarhey.struk@gmail.com>
2018-03-16 00:53:53 +0300

    first commit

---
+++ GAClibTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using GAClib;
+
+namespace GAClibTest
+{
+    public class DemonstrationClass
+    {
+        TestClass test = new TestClass();
+
+        public void DemonstrationRandomMethod()
+        {
+            string s = test.RandomMethod();
+        }
+
+        public void DemonstationRandomMethodWithParams()
+        {
+            string s = test.RandomMethodWithParams(15, "Hello");
+        }
+    }
+}
---
+++ GAClibTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GAClibTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GAClibTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("92f24849-8ff1-4407-bf05-17a0b0e3084a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
